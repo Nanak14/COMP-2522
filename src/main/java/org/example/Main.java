6129b0cf6507c32bec1a7884256d1e9acac80f1d
Nanakpreet Singh <snanak1403@gmail.com>
2022-09-11 20:03:40 -0700

    Changed the hello world message.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!, My name is Nanakpreet Singh");
+        System.out.println("Hello world!, This is my world");
     }
 }
